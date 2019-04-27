977921f826eeaf69b4e0cac94c9b5e304f9c61be
Ali Maisam <alimaisam@seekasia.com>
2019-04-27 17:30:07 +0800

    modify response body from github api

--- src/search-service/store.js
+++ src/search-service/store.js
@@ -1,15 +1,35 @@
+import axios from 'axios'
+import circularJSON from 'circular-json'
+
 export default function Store () {
     async function language (keyword, page = 1, per_page = 10) {
         if (!keyword)
             return null;
-        console.log(keyword);
-      //const [rows] = await db.query('SELECT * FROM food WHERE id = ?', [id])
-      //return (rows && rows[0]) || null
+
+        const url = `https://api.github.com/search/repositories?q=language:${keyword}&page=${page}&per_page=${per_page}`
+        const response = await axios.get(url)
+        const result = {
+            total_count: response.data.total_count,
+            page,
+            per_page,
+            items: response.data.items
+        }
+        return result;
     }
   
     async function topic (keyword, page = 1, per_page = 10 ) {
-      //const [rows] = await db.query('SELECT * from food')
-      //return rows
+        if (!keyword)
+            return null;
+
+        const url = `https://api.github.com/search/repositories?q=topic:${keyword}&page=${page}&per_page=${per_page}`
+        const response = await axios.get(url)
+        const result = {
+            total_count: response.data.total_count,
+            page,
+            per_page,
+            items: response.data.items
+        }
+        return result;
     }
   
     return {
